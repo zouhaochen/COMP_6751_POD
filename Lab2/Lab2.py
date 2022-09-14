18954ca79930e66cb109b320694fd8289b3fda89
NormalZHC <zouhaochenluke@gmail.com>
2022-09-14 10:53:50 -0400

    Lab2 update

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -2,17 +2,27 @@
 #    They wind back the clock, while we chase after the wind.
 #    Use the NLTK Ch5 POS tagset for this exercise.
 #    Solution:
-#    They: PRON (pronoun)
-#    wind: VERB (verb)
-#    back: ADP (adposition)
-#    the: DET (determiner)
-#    clock: NOUN (noun)
-#    ,: PUNCT (punctuation)
-#    while: CCONJ (coordinating conjunction)
-#    we: PRON (pronoun)
-#    chase: VERB (verb)
-#    after: ADP (adposition)
-#    the: DET (determiner)
-#    wind: NOUN (noun)
-#    .: PUNCT (punctuation)
+#    (This solution is based on the Universal POS tagset: https://universaldependencies.org/u/pos/)
+#    (There are also another tagset Penn Treebank: https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html)
+#    They:  PRON    (pronoun)
+#    wind:  VERB    (verb)
+#    back:  ADP     (adposition)
+#    the:   DET     (determiner)
+#    clock: NOUN    (noun)
+#    ,:     PUNCT   (punctuation)
+#    while: CCONJ   (coordinating conjunction)
+#    we:    PRON    (pronoun)
+#    chase: VERB    (verb)
+#    after: ADP     (adposition)
+#    the:   DET     (determiner)
+#    wind:  NOUN    (noun)
+#    .:     PUNCT   (punctuation)
+
+
+
+
+
+
+
+
 
