d893917d6eb1dcc04b523f0eadf387ead83cdb39
NormalZHC <zouhaochenluke@gmail.com>
2022-09-14 10:47:50 -0400

    Lab2 update

---
+++ Lab2/Lab2.py
@@ -0,0 +1,18 @@
+# 1. manually assign all possible POS tags for each of the words in the following sentence:
+#    They wind back the clock, while we chase after the wind.
+#    Use the NLTK Ch5 POS tagset for this exercise.
+#    Solution:
+#    They: PRON (pronoun)
+#    wind: VERB (verb)
+#    back: ADP (adposition)
+#    the: DET
+#    clock: NOUN
+#    ,: PUNCT
+#    while: CCONJ
+#    we: PRON
+#    chase: VERB
+#    after: ADP
+#    the: DET
+#    wind: NOUN
+#    .: PUNCT
+
